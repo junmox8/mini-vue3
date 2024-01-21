778a5881b322e791f22fc868364757fa6b5bb362
huangjunyi <huangjunyi@baidu.com>
2024-01-21 19:09:31 +0800

    feat: 新增shapeFlag

--- src/runtime-core/render.ts
+++ src/runtime-core/render.ts
@@ -1,13 +1,15 @@
 import { createComponentInstance, setupComponent } from "./component";
 import { isObject } from "../shared";
+import { ShapeFlags } from "./ShapeFlags";
 export function render(vnode, container) {
   patch(vnode, container);
 }
 
 function patch(vnode, container) {
-  if (typeof vnode.type === "string") {
+  const { shapeFlag } = vnode;
+  if (shapeFlag & ShapeFlags.ELEMENT) {
     processElement(vnode, container);
-  } else if (isObject(vnode.type)) {
+  } else if (shapeFlag & ShapeFlags.STATEFUL_COMPONENT) {
     processComponent(vnode, container);
   }
 }
@@ -21,16 +23,16 @@ function processComponent(vnode, container) {
 }
 
 function mountElement(vnode, container) {
-  const { type, props, children } = vnode;
+  const { type, props, children, shapeFlag } = vnode;
   const el = (vnode.el = document.createElement(type)); //进行vnode的el赋值
   if (props) {
     for (let key in props) {
       el.setAttribute(key, props[key]);
     }
   }
-  if (typeof children === "string") {
+  if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
     el.textContent = children;
-  } else if (Array.isArray(children)) {
+  } else if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
     mountChildren(children, el);
   }
   container.append(el);
--- src/runtime-core/vnode.ts
+++ src/runtime-core/vnode.ts
@@ -1,9 +1,24 @@
+import { ShapeFlags } from "./ShapeFlags";
+
 export function createVNode(type, children?, props?) {
   const vnode = {
     type,
     props,
     children,
     el: null, //方便后续$el取值
+    shapeFlag: getShapeFlag(type),
   };
+  if (typeof children === "string") {
+    vnode.shapeFlag |= ShapeFlags.TEXT_CHILDREN;
+  } else if (Array.isArray(children)) {
+    vnode.shapeFlag |= ShapeFlags.ARRAY_CHILDREN;
+  }
+
   return vnode;
+}
+
+function getShapeFlag(type) {
+  return typeof type === "string"
+    ? ShapeFlags.ELEMENT
+    : ShapeFlags.STATEFUL_COMPONENT;
 }
