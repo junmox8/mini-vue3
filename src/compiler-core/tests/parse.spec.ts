6c284cae926bfcef517c0ea107a4d8f1c23062f1
root <931097192@qq.com>
2024-02-15 01:15:40 +0800

    feat: 新增解析插值功能

---
+++ src/compiler-core/tests/parse.spec.ts
@@ -0,0 +1,24 @@
+/*
+ * @Author: root 931097192@qq.com
+ * @Date: 2024-02-11 14:19:58
+ * @LastEditors: root 931097192@qq.com
+ * @LastEditTime: 2024-02-15 01:13:22
+ * @FilePath: \mini-vue\writing-vue3\src\compiler-core\tests\parse.spec.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import { baseParse } from "../parse";
+import { NodeTypes } from "../ast";
+describe("parse", () => {
+  describe("interpolation", () => {
+    it("simple interpolation", () => {
+      const ast = baseParse("{{ message }}");
+      expect(ast.children[0]).toStrictEqual({
+        type: NodeTypes.INTERPOLATION,
+        content: {
+          type: NodeTypes.SIMPLE_EXPRESSION,
+          content: "message",
+        },
+      });
+    });
+  });
+});
